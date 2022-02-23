cf0ce87320cd50a7b0b5fcdeb01dcace5244c6b5
REGO350 <riokanehiro228pa@gmail.com>
2022-02-23 15:07:12 +0900

    remame

--- contracts/mocks/Usdc.sol
+++ contracts/mocks/Usdc.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract USDC is ERC20 {
+contract MockUsdc is ERC20 {
   constructor() ERC20("USD Coin", "USDC") {
     _mint(msg.sender, 1_000_000e6);
   }
