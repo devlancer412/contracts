5c97fa53ff0e9213b0b4c3c713037d38722e2928
REGO350 <riokanehiro228pa@gmail.com>
2022-03-02 16:58:47 +0900

    removed base stats

--- contracts/Rooster.sol
+++ contracts/Rooster.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import {Pausable} from "@openzeppelin/contracts/security/Pausable.sol";
+import {ERC721} from "@rari-capital/solmate/src/tokens/ERC721.sol";
 import "./RoosterMetadata.sol";
 import "./AccessControl.sol";
 import "hardhat/console.sol";
@@ -10,8 +9,6 @@ import "hardhat/console.sol";
 contract Rooster is ERC721, AccessControl, RoosterMetadata {
   //Current roosterId count
   uint256 private _roosterIdCounter = 0;
-  //Rooster hard cap
-  uint256 public constant cap = 150_000;
 
   constructor(string memory baseUri_) ERC721("Rooster", "ROOSTER") {
     setBaseUri(baseUri_);
@@ -22,19 +19,27 @@ contract Rooster is ERC721, AccessControl, RoosterMetadata {
   }
 
   function mint(address to, Breed breed) external onlyMinter {
-    require(totalSupply() + 1 <= cap, "cap exceeded");
-    uint256 roosterId = _roosterIdCounter++;
+    uint256 roosterId = _roosterIdCounter;
+
+    unchecked {
+      _roosterIdCounter++;
+    }
+
     _mint(to, roosterId, breed);
   }
 
   function mintBatch(address to, Breed[] memory breeds) external onlyMinter {
-    require(totalSupply() + breeds.length <= cap, "cap exceeded");
-
     uint256 roosterId = _roosterIdCounter;
-    for (uint256 i = 0; i < breeds.length; i++) {
+
+    for (uint256 i = 0; i < breeds.length; ) {
       _mint(to, roosterId, breeds[i]);
-      roosterId++;
+
+      unchecked {
+        roosterId++;
+        i++;
+      }
     }
+
     _roosterIdCounter = roosterId;
   }
 
@@ -45,9 +50,5 @@ contract Rooster is ERC721, AccessControl, RoosterMetadata {
   ) private {
     _safeMint(to, roosterId);
     _setBreed(roosterId, breed);
-  }
-
-  function _baseURI() internal view override(ERC721, RoosterMetadata) returns (string memory) {
-    return RoosterMetadata._baseURI();
   }
 }
--- contracts/RoosterMetadata.sol
+++ contracts/RoosterMetadata.sol
@@ -1,24 +1,24 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@rari-capital/solmate/src/tokens/ERC721.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
 import "./AccessControl.sol";
 
 abstract contract RoosterMetadata is ERC721, AccessControl {
+  using Strings for uint256;
+
   //Rooster metadata base uri
   string private _baseUri;
   //Maps rooster id to breed
   mapping(uint256 => Breed) private _breeds;
-  //Maps breed to base stats
-  mapping(Breed => BaseStats) public baseStats;
 
-  //Fires when base stats is updated
-  event UpdateBaseStats(Breed breed, BaseStats baseStats);
   //Fires when base uri is updated
   event UpdateBaseUri(string baseUri);
   //Fires when breed is set
   event BreedSet(uint256 roosterId, Breed breed);
 
+  //May change to uint256 for scalability
   enum Breed {
     Swansons,
     Kelians,
@@ -32,23 +32,14 @@ abstract contract RoosterMetadata is ERC721, AccessControl {
     Henis
   }
 
-  struct BaseStats {
-    uint32 VIT;
-    uint32 WATK;
-    uint32 BATK;
-    uint32 CATK;
-    uint32 SPD;
-    uint32 AGRO;
-  }
-
   function breeds(uint256 roosterId) public view returns (Breed) {
     require(_exists(roosterId), "Query for nonexistent rooster");
     return _breeds[roosterId];
   }
 
-  function setBaseStats(Breed breed, BaseStats memory newStats) external onlyOwner {
-    baseStats[breed] = newStats;
-    emit UpdateBaseStats(breed, newStats);
+  function tokenURI(uint256 roosterId) public view override returns (string memory) {
+    require(_exists(roosterId), "Query for nonexistent rooster");
+    return string(abi.encodePacked(_baseUri, roosterId.toString()));
   }
 
   function setBaseUri(string memory newUri) public onlyOwner {
@@ -56,12 +47,12 @@ abstract contract RoosterMetadata is ERC721, AccessControl {
     emit UpdateBaseUri(newUri);
   }
 
-  function _baseURI() internal view virtual override returns (string memory) {
-    return _baseUri;
-  }
-
   function _setBreed(uint256 roosterId, Breed breed) internal {
     _breeds[roosterId] = breed;
     emit BreedSet(roosterId, breed);
+  }
+
+  function _exists(uint256 tokenId) internal view virtual returns (bool) {
+    return ownerOf[tokenId] != address(0);
   }
 }
