f9d1f0c55dd0a9354c3aecd7c417fc37e640c4d5
REGO350 <riokanehiro228pa@gmail.com>
2022-03-03 10:18:29 +0900

    rename

--- deploy/00_mocks.ts
+++ deploy/00_mocks.ts
@@ -1,10 +1,10 @@
 import { DeployFunction } from "hardhat-deploy/types";
-import { USDC__factory } from "../types";
+import { MockUsdc__factory } from "../types";
 import { toWei, Ship } from "../utils";
 
 const func: DeployFunction = async (hre) => {
   const { deploy, users } = await Ship.init(hre);
-  const usdc = await deploy(USDC__factory);
+  const usdc = await deploy(MockUsdc__factory);
   for (let i = 1; i < 10; i++) {
     await usdc.transfer(users[i].address, toWei(1000, 6));
   }
