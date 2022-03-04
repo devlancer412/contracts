6c0365823d724b49be9cc1509df9aa0b7a1a2ed5
REGO350 <riokanehiro228pa@gmail.com>
2022-03-04 14:37:49 +0900

    foundry test

--- shell.nix
+++ shell.nix
@@ -1,17 +1,17 @@
-# Copied from https://github.com/centrifuge/tinlake-maker-lib/blob/master/shell.nix
+# # Copied from https://github.com/centrifuge/tinlake-maker-lib/blob/master/shell.nix
 
-let
-  pkgs = import (builtins.fetchGit rec {
-    name = "dapptools-${rev}";
-    url = https://github.com/dapphub/dapptools;
-    rev = "d7a23096d8ae8391e740f6bdc4e8b9b703ca4764";
-  }) {};
+# let
+#   pkgs = import (builtins.fetchGit rec {
+#     name = "dapptools-${rev}";
+#     url = https://github.com/dapphub/dapptools;
+#     rev = "d7a23096d8ae8391e740f6bdc4e8b9b703ca4764";
+#   }) {};
 
-in
-  pkgs.mkShell {
-    src = null;
-    buildInputs = with pkgs; [
-      pkgs.dapp
-      pkgs.solc-static-versions.solc_0_8_9
-    ];
-  }
+# in
+#   pkgs.mkShell {
+#     src = null;
+#     buildInputs = with pkgs; [
+#       pkgs.dapp
+#       pkgs.solc-static-versions.solc_0_8_9
+#     ];
+#   }
