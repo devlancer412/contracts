18fd3fe5505b8fb64d61874b9266e487ebf46b2b
regohiro.eth <62818999+regohiro@users.noreply.github.com>
2022-04-02 15:23:45 +0900

    auth test

--- contracts/Auth.sol
+++ contracts/Auth.sol
@@ -11,24 +11,24 @@ library Strings {
   }
 }
 
-contract RoosterAuth {
+contract Auth {
   //Address of current owner
   address public owner;
   //Address of new owner (Note: new owner must pull to be an owner)
   address public newOwner;
   //If paused or not
   uint256 private _paused;
-
-  mapping(bytes32 => mapping(address => bool)) private roles;
+  //Roles mapping (role => address => has role)
+  mapping(bytes32 => mapping(address => bool)) private _roles;
 
   //Fires when a new owner is pushed
   event OwnerPushed(address indexed pushedOwner);
   //Fires when new owner pulled
   event OwnerPulled(address indexed previousOwner, address indexed newOwner);
   //Fires when account is granted role
-  event RoleGranted(string indexed role, address indexed account, address sender);
+  event RoleGranted(string indexed role, address indexed account, address indexed sender);
   //Fires when accoount is revoked role
-  event RoleRevoked(string indexed role, address indexed account, address sender);
+  event RoleRevoked(string indexed role, address indexed account, address indexed sender);
   //Fires when pause is triggered by account
   event Paused(address account);
   //Fires when pause is lifted by account
@@ -64,7 +64,11 @@ contract RoosterAuth {
   }
 
   function hasRole(string memory role, address account) public view returns (bool) {
-    return roles[bytes32(bytes(role))][account];
+    return _roles[bytes32(bytes(role))][account];
+  }
+
+  function paused() public view returns (bool) {
+    return _paused == 1 ? true : false;
   }
 
   function pushOwner(address account) public onlyOwner {
@@ -97,24 +101,6 @@ contract RoosterAuth {
     _revokeRole(role, msg.sender);
   }
 
-  function _grantRole(string calldata role, address account) private {
-    if (!hasRole(role, account)) {
-      bytes32 encodedRole = Strings.toBytes32(role);
-      roles[encodedRole][account] = true;
-      emit RoleGranted(role, account, msg.sender);
-    }
-  }
-
-  function _revokeRole(string calldata role, address account) private {
-    bytes32 encodedRole = Strings.toBytes32(role);
-    roles[encodedRole][account] = false;
-    emit RoleRevoked(role, account, msg.sender);
-  }
-
-  function paused() public view returns (bool) {
-    return _paused == 1 ? true : false;
-  }
-
   function pause() external onlyRole("PAUSER") whenNotPaused {
     _paused = 1;
     emit Paused(msg.sender);
@@ -123,5 +109,19 @@ contract RoosterAuth {
   function unpause() external onlyRole("PAUSER") whenPaused {
     _paused = 0;
     emit Unpaused(msg.sender);
+  }
+
+  function _grantRole(string calldata role, address account) private {
+    if (!hasRole(role, account)) {
+      bytes32 encodedRole = Strings.toBytes32(role);
+      _roles[encodedRole][account] = true;
+      emit RoleGranted(role, account, msg.sender);
+    }
+  }
+
+  function _revokeRole(string calldata role, address account) private {
+    bytes32 encodedRole = Strings.toBytes32(role);
+    _roles[encodedRole][account] = false;
+    emit RoleRevoked(role, account, msg.sender);
   }
 }
---
+++ forge_test/src/Auth.t.sol
@@ -0,0 +1,229 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity ^0.8.9;
+
+import {Auth} from "contracts/Auth.sol";
+import "./utils/BasicSetup.sol";
+
+interface AuthEvent {
+  event OwnerPushed(address indexed pushedOwner);
+  event OwnerPulled(address indexed previousOwner, address indexed newOwner);
+  event RoleGranted(string indexed role, address indexed account, address indexed sender);
+  event RoleRevoked(string indexed role, address indexed account, address indexed sender);
+  event Paused(address account);
+  event Unpaused(address account);
+}
+
+contract AuthTest is BasicSetup, AuthEvent {
+  Auth auth;
+
+  function setUp() public {
+    vm.expectEmit(true, true, false, false);
+    emit OwnerPulled(address(0), address(this));
+    auth = new Auth();
+  }
+
+  function testInit() public {
+    assertEq(auth.owner(), address(this));
+    assertEq(auth.newOwner(), address(0));
+    assertTrue(auth.paused() == false);
+  }
+
+  function testPushOwner(address user) public {
+    vm.assume(user != address(0) && user != address(this));
+
+    //Push
+    vm.expectEmit(true, false, false, false);
+    emit OwnerPushed(user);
+    auth.pushOwner(user);
+
+    assertEq(auth.owner(), address(this));
+    assertEq(auth.newOwner(), user);
+  }
+
+  function testCannotPushOwnerIfNotOwner(address user) public {
+    vm.assume(user != address(this));
+
+    vm.prank(user);
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "OWNER", user));
+    auth.pushOwner(user);
+  }
+
+  function testCannotPushAddressZero() public {
+    vm.expectRevert(bytes("No address(0)"));
+    auth.pushOwner(address(0));
+  }
+
+  function testCannotPushCurrentOwner() public {
+    vm.expectRevert(bytes("Only new owner"));
+    auth.pushOwner(address(this));
+  }
+
+  function testPullOwner(address user) public {
+    vm.assume(user != address(0) && user != address(this));
+
+    //Push
+    auth.pushOwner(user);
+
+    //Pull
+    vm.prank(user);
+    vm.expectEmit(true, true, false, false);
+    emit OwnerPulled(address(this), user);
+    auth.pullOwner();
+
+    assertEq(auth.owner(), user);
+    assertEq(auth.newOwner(), user);
+  }
+
+  function testCannotPullIfNotNewOwner(address user1, address user2) public {
+    vm.assume(user1 != user2);
+
+    //Push
+    auth.pushOwner(user1);
+
+    //Pull
+    vm.prank(user2);
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "NEW_OWNER", user2));
+    auth.pullOwner();
+  }
+
+  function testGrantRole(string calldata role, address user) public {
+    vm.assume(bytes(role).length <= 32 && bytes(role).length > 0 && user != address(0));
+
+    vm.expectEmit(true, true, true, false);
+    emit RoleGranted(role, user, address(this));
+    auth.grantRole(role, user);
+    assertTrue(auth.hasRole(role, user) == true);
+  }
+
+  function testCannotGrantRoleIfNotOwner(address user) public {
+    vm.assume(user != address(this));
+
+    vm.prank(user);
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "OWNER", user));
+    auth.grantRole("KING", user);
+  }
+
+  function testCannotGrantRoleIfNoRoleIsGiven(address user) public {
+    vm.expectRevert(bytes("Role not given"));
+    auth.grantRole("", user);
+  }
+
+  function testCannotGrantAddressZero() public {
+    vm.expectRevert(bytes("No address(0)"));
+    auth.grantRole("KING", address(0));
+  }
+
+  function testRevokeRole(string calldata role, address user) public {
+    vm.assume(bytes(role).length <= 32 && bytes(role).length > 0 && user != address(0));
+
+    //Grant
+    auth.grantRole(role, user);
+
+    //Revoke
+    vm.expectEmit(true, true, true, false);
+    emit RoleRevoked(role, user, address(this));
+    auth.revokeRole(role, user);
+    assertTrue(auth.hasRole(role, user) == false);
+  }
+
+  function testCannotRevokeRoleIfNotOwner(address user) public {
+    auth.grantRole("KING", alice);
+
+    vm.prank(user);
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "OWNER", user));
+    auth.revokeRole("KING", alice);
+  }
+
+  function testCannotRevokeIfRoleNotGiven(string memory role1, string memory role2) public {
+    vm.assume(bytes(role1).length <= 32 && bytes(role1).length > 0);
+    vm.assume(bytes(role2).length <= 32 && bytes(role2).length > 0);
+    vm.assume(keccak256(bytes(role1)) != keccak256(bytes(role2)));
+
+    //Grant
+    auth.grantRole(role1, alice);
+
+    //Revoke
+    vm.expectRevert(bytes("Role not granted"));
+    auth.revokeRole(role2, alice);
+  }
+
+  function testRenounceRole(string calldata role, address user) public {
+    vm.assume(bytes(role).length <= 32 && bytes(role).length > 0 && user != address(0));
+
+    //Grant
+    auth.grantRole(role, user);
+
+    //Renounce
+    vm.expectEmit(true, true, true, false);
+    emit RoleRevoked(role, user, user);
+    vm.prank(user);
+    auth.renounceRole(role);
+    assertTrue(auth.hasRole(role, user) == false);
+  }
+
+  function testCannotRenounceIfRoleNotGiven(string memory role1, string memory role2) public {
+    vm.assume(bytes(role1).length <= 32 && bytes(role1).length > 0);
+    vm.assume(bytes(role2).length <= 32 && bytes(role2).length > 0);
+    vm.assume(keccak256(bytes(role1)) != keccak256(bytes(role2)));
+
+    //Grant
+    auth.grantRole(role1, alice);
+
+    //Renounce
+    vm.expectRevert(bytes("Role not granted"));
+    vm.prank(alice);
+    auth.renounceRole(role2);
+  }
+
+  function grantPauserRole() public {
+    grantPauserRole(address(this));
+  }
+
+  function grantPauserRole(address user) public {
+    auth.grantRole("PAUSER", user);
+  }
+
+  function testPause() public {
+    grantPauserRole();
+
+    vm.expectEmit(false, false, false, true);
+    emit Paused(address(this));
+    auth.pause();
+    assertTrue(auth.paused());
+  }
+
+  function testCannotPauseIfPaused() public {
+    grantPauserRole();
+    auth.pause();
+
+    vm.expectRevert(abi.encodeWithSelector(Auth.IsPaused.selector));
+    auth.pause();
+  }
+
+  function testCannotPauseIfNotPauser() public {
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "PAUSER", address(this)));
+    auth.pause();
+  }
+
+  function testUnpause() public {
+    grantPauserRole();
+    auth.pause();
+
+    vm.expectEmit(false, false, false, true);
+    emit Unpaused(address(this));
+    auth.unpause();
+    assertTrue(!auth.paused());
+  }
+
+  function testCannotUnpauseIfUnpaused() public {
+    grantPauserRole();
+
+    vm.expectRevert(abi.encodeWithSelector(Auth.NotPaused.selector));
+    auth.unpause();
+  }
+
+  function testCannotUnpauseIfNotPauser() public {
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "PAUSER", address(this)));
+    auth.unpause();
+  }
+}
