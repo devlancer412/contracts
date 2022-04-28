9356b868abe46f1d3be9568024e5111a25838050
regohiro.eth <62818999+regohiro@users.noreply.github.com>
2022-04-28 21:58:36 +0900

    fixed calculation

--- contracts/gwit/GwitRedeemer.sol
+++ contracts/gwit/GwitRedeemer.sol
@@ -2,72 +2,83 @@
 pragma solidity ^0.8.9;
 
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import {IERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/draft-IERC20Permit.sol";
 import {Auth} from "../utils/Auth.sol";
 
-interface IpGwit is IERC20, IERC20Permit {}
-
-contract GwitRedeemer is Auth {
-  IpGwit public immutable pGwit;
-  IERC20 public immutable gwit;
+contract GwitVesting is Auth {
+  //Gwit vesting info
   Info public info;
-
-  mapping(address => uint32) public lastRedeemed;
-
-  struct Sig {
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-  }
+  //pGWIT address
+  IERC20 public immutable pGwit;
+  //GWIT address
+  IERC20 public immutable gwit;
+  //User redeemed amount
+  mapping(address => uint256) public redeemed;
 
   struct Info {
+    //start time of the vesting period
     uint32 start;
+    //cliff period in seconds
+    uint32 cliff;
+    //duration of vesting period
     uint32 duration;
+    //initial release at start time in hundreds ( 500 = 5% )
+    uint32 initialRate;
+    //total supply of tokens to be released
     uint256 supply;
+    //amount of tokens released
     uint256 released;
   }
 
   event Redeem(address indexed user, address indexed recipient, uint256 amount);
   event Set(uint256 start, uint256 duration, uint256 supply);
 
+  error ExceedsLimit(uint256 limit);
+  error ExceedsSupply();
+
   constructor(address pGwit_, address gwit_) {
-    pGwit = IpGwit(pGwit_);
+    pGwit = IERC20(pGwit_);
     gwit = IERC20(gwit_);
   }
 
   function redeemable(address user) public view returns (uint256) {
-    uint32 start = lastRedeemed[user] > 0 ? lastRedeemed[user] : info.start;
-    if (start < block.timestamp) {
+    return vestedAmount(user) - redeemed[user];
+  }
+
+  function vestedAmount(address user) public view returns (uint256) {
+    uint256 total = pGwit.balanceOf(user);
+    uint256 currentTime = block.timestamp;
+    Info memory _info = info;
+
+    if (currentTime < _info.start) {
       return 0;
+    } else if (currentTime < _info.start + _info.duration) {
+      uint256 initial = (total * _info.initialRate) / 10_000;
+      if (currentTime < _info.start + _info.cliff) {
+        return initial;
+      } else {
+        return ((currentTime - _info.start) * (total - initial)) / _info.duration + initial;
+      }
     } else {
-      return ((block.timestamp - start) * pGwit.balanceOf(user)) / info.duration;
+      return total;
     }
   }
 
-  function redeem(
-    address recipient,
-    uint256 amount,
-    uint256 deadline,
-    Sig calldata sig
-  ) external {
+  function redeem(address recipient, uint256 amount) external whenNotPaused {
     address user = msg.sender;
-    uint256 redeemableAmount = redeemable(user);
-    if (amount > 0) {
-      require(amount <= redeemableAmount, "Exceeds limit");
-    } else {
-      amount = redeemableAmount;
-    }
+    uint256 maxAmount = redeemable(user);
 
-    lastRedeemed[user] = uint32(block.timestamp);
+    if (amount > 0) {
+      if (amount > maxAmount) revert ExceedsLimit(maxAmount);
+    } else {
+      amount = maxAmount;
+    }
+    if (amount > info.supply - info.released) revert ExceedsSupply();
+
     unchecked {
       info.released += amount;
+      redeemed[user] += amount;
     }
 
-    if (deadline > 0) {
-      pGwit.permit(user, address(this), amount, deadline, sig.v, sig.r, sig.s);
-    }
-
-    pGwit.transferFrom(user, address(this), amount);
     gwit.transfer(recipient, amount);
 
     emit Redeem(user, recipient, amount);
@@ -75,14 +86,18 @@ contract GwitRedeemer is Auth {
 
   function set(
     uint32 start,
+    uint32 cliff,
     uint32 duration,
+    uint32 initialRate,
     uint256 supply
   ) external onlyOwner {
     Info memory _info = info;
 
     _info.start = start;
+    _info.cliff = cliff;
     _info.duration = duration;
     _info.supply = supply;
+    _info.initialRate = initialRate;
     info = _info;
 
     emit Set(start, duration, supply);
