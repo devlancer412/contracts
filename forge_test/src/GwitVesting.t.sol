45cca35135ae1a9887633c1822b46b5a058e132c
regohiro.eth <62818999+regohiro@users.noreply.github.com>
2022-05-02 18:35:30 +0900

    more tests

--- forge_test/src/GwitVesting.t.sol
+++ forge_test/src/GwitVesting.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import {GwitVestingSetup} from "./utils/GwitVestingSetup.sol";
+import "./utils/GwitVestingSetup.sol";
 
 contract GwitVestingTest is GwitVestingSetup {
   function setUp() public override {
@@ -16,12 +16,12 @@ contract GwitVestingTest is GwitVestingSetup {
     assertEq(vesting.vestedAmount(alice), 0);
   }
 
-  function testVestedAmountAfterTGE(uint88 aGwitAmount) public {
+  function testVestedAmountAfterTGE(uint88 aGwitAmount, uint32 delta) public {
     vm.assume(aGwitAmount >= 1e16 && aGwitAmount <= 100_000e18);
     aGwit.mint(alice, aGwitAmount);
 
     set();
-    gotoTGE();
+    gotoTGE(delta);
 
     (, , , uint32 initialRate) = vesting.info();
     assertEq(vesting.vestedAmount(alice), (aGwitAmount * initialRate) / 10_000);
@@ -34,8 +34,7 @@ contract GwitVestingTest is GwitVestingSetup {
     aGwit.mint(alice, aGwitAmount);
 
     set();
-    gotoAfterCliff();
-    skip(delta);
+    gotoAfterCliff(10000);
 
     (uint32 startTime, , uint32 duration, uint32 initialRate) = vesting.info();
     uint256 initialAmount = (aGwitAmount * initialRate) / 10_000;
@@ -44,5 +43,129 @@ contract GwitVestingTest is GwitVestingSetup {
       vesting.vestedAmount(alice),
       ((currentTime - startTime) * (aGwitAmount - initialAmount)) / duration + initialAmount
     );
+    assertEq(
+      vesting.redeemable(alice),
+      ((currentTime - startTime) * (aGwitAmount - initialAmount)) / duration + initialAmount
+    );
+  }
+
+  function testVestAmountAfterFullyVested(uint88 aGwitAmount, uint32 delta) public {
+    vm.assume(aGwitAmount >= 1e16 && aGwitAmount <= 100_000e18);
+    aGwit.mint(alice, aGwitAmount);
+
+    set();
+    gotoFullyVested(delta);
+
+    assertEq(vesting.vestedAmount(alice), aGwitAmount);
+    assertEq(vesting.redeemable(alice), aGwitAmount);
+  }
+
+  function testRedeemOnTGE(uint88 aGwitAmount, uint32 delta) public {
+    vm.assume(aGwitAmount >= 1e16 && aGwitAmount <= 100_000e18);
+    aGwit.mint(alice, aGwitAmount);
+
+    set();
+    (, , , uint32 initialRate) = vesting.info();
+
+    gotoTGE(delta);
+    vm.prank(alice);
+    vm.expectEmit(true, true, false, true);
+    emit Redeem(alice, alice, (aGwitAmount * initialRate) / 10_000);
+    vesting.redeem(alice, 0);
+
+    assertEq(vesting.vestedAmount(alice), (aGwitAmount * initialRate) / 10_000);
+    assertEq(vesting.redeemable(alice), 0);
+    assertEq(gwit.balanceOf(alice), (aGwitAmount * initialRate) / 10_000);
+    assertEq(aGwit.balanceOf(alice), aGwitAmount);
+  }
+
+  function testRedeemOnTGEAndAfterCliff(uint88 aGwitAmount, uint32 delta) public {
+    vm.assume(aGwitAmount >= 1e16 && aGwitAmount <= 100_000e18);
+    aGwit.mint(alice, aGwitAmount);
+
+    set();
+    (uint32 startTime, , uint32 duration, uint32 initialRate) = vesting.info();
+
+    gotoTGE(0);
+    vm.startPrank(alice);
+    vesting.redeem(alice, 0);
+
+    gotoAfterCliff(delta);
+    uint32 currentTime = uint32(block.timestamp);
+    uint256 initial = (aGwitAmount * initialRate) / 10_000;
+    uint256 current = ((currentTime - startTime) * (aGwitAmount - initial)) / duration;
+
+    assertEq(vesting.redeemable(alice), current);
+    vm.expectEmit(true, true, false, true);
+    emit Redeem(alice, alice, current);
+    vesting.redeem(alice, 0);
+
+    assertEq(vesting.vestedAmount(alice), initial + current);
+    assertEq(vesting.redeemable(alice), 0);
+    assertEq(gwit.balanceOf(alice), initial + current);
+    assertEq(aGwit.balanceOf(alice), aGwitAmount);
+  }
+
+  function testRedeemOnTGEAndAfterFullyVested(uint88 aGwitAmount, uint32 delta) public {
+    vm.assume(aGwitAmount >= 1e16 && aGwitAmount <= 100_000e18);
+    aGwit.mint(alice, aGwitAmount);
+
+    set();
+    (, , , uint32 initialRate) = vesting.info();
+
+    gotoTGE(10);
+    vm.startPrank(alice);
+    vesting.redeem(alice, 0);
+
+    gotoFullyVested(delta);
+    uint256 initial = (aGwitAmount * initialRate) / 10_000;
+    uint256 current = aGwitAmount - initial;
+
+    assertEq(vesting.redeemable(alice), current);
+    vm.expectEmit(true, true, false, true);
+    emit Redeem(alice, alice, current);
+    vesting.redeem(alice, 0);
+
+    assertEq(vesting.vestedAmount(alice), initial + current);
+    assertEq(vesting.redeemable(alice), 0);
+    assertEq(gwit.balanceOf(alice), initial + current);
+    assertEq(aGwit.balanceOf(alice), aGwitAmount);
+  }
+
+  function testCannotRedeemWhenItsPaused() public {
+    set();
+    vesting.grantRole("PAUSER", address(this));
+    vesting.pause();
+    vm.expectRevert(abi.encodeWithSelector(Auth.IsPaused.selector));
+    vesting.redeem(alice, 1e18);
+  }
+
+  function testWithdraw(uint128 amount) public {
+    vm.assume(amount <= 50_000_000e18);
+    vesting.grantRole("VAULT", address(this));
+
+    set();
+
+    vm.expectEmit(true, false, false, true);
+    emit Withdraw(address(this), amount, 50_000_000e18 - amount);
+    vesting.withdraw(amount);
+    assertEq(gwit.balanceOf(address(this)), amount);
+  }
+
+  function testCannotWithdrawIfNotVault() public {
+    vm.expectRevert(abi.encodeWithSelector(Auth.Unauthorized.selector, "VAULT", address(this)));
+    vesting.withdraw(1e18);
+  }
+
+  function testCannotWithdrawDuringVestingPeriod(uint32 delta) public {
+    set();
+    vesting.grantRole("VAULT", address(this));
+
+    (uint32 startTime, , uint32 duration, ) = vesting.info();
+    vm.assume(delta < duration);
+    vm.warp(startTime + delta);
+
+    vm.expectRevert(abi.encodeWithSelector(GwitVesting.CannotWithdrawDuringVestingPeriod.selector));
+    vesting.withdraw(1e18);
   }
 }
--- forge_test/src/utils/GwitVestingSetup.sol
+++ forge_test/src/utils/GwitVestingSetup.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.9;
 
 import {GwitVesting} from "contracts/gwit/GwitVesting.sol";
 import {MockERC20} from "contracts/mocks/MockERC20.sol";
+import {Auth} from "contracts/utils/Auth.sol";
 import {BasicSetup} from "./BasicSetup.sol";
 
 contract GwitVestingSetup is BasicSetup {
+  event Redeem(address indexed user, address indexed recipient, uint256 amount);
+  event Set(uint32 start, uint32 cliff, uint32 duration, uint32 initialRate);
+  event Withdraw(address indexed user, uint256 amount, uint256 balance);
+
   GwitVesting vesting;
   MockERC20 aGwit;
   MockERC20 gwit;
@@ -14,6 +19,10 @@ contract GwitVestingSetup is BasicSetup {
     aGwit = new MockERC20("AlphaGwit", "aGWIT", 18);
     gwit = new MockERC20("Gwit", "GWIT", 18);
     vesting = new GwitVesting(address(aGwit), address(gwit));
+
+    vm.label(address(aGwit), "aGWIT");
+    vm.label(address(gwit), "GWIT");
+    vm.label(address(vesting), "Vesting");
 
     gwit.mint(address(vesting), 50_000_000e18);
   }
@@ -27,18 +36,21 @@ contract GwitVestingSetup is BasicSetup {
     vesting.set(TGE, cliff, duration, initialRate);
   }
 
-  function gotoTGE() public virtual {
-    (uint32 startTime, , , ) = vesting.info();
-    vm.warp(startTime);
-  }
-
-  function gotoAfterCliff() public virtual {
+  function gotoTGE(uint32 delta) public virtual {
     (uint32 startTime, uint32 cliff, , ) = vesting.info();
-    vm.warp(startTime + cliff);
+    vm.assume(delta < cliff);
+    vm.warp(startTime + delta);
   }
 
-  function gotoFullyVested() public virtual {
+  function gotoAfterCliff(uint32 delta) public virtual {
+    (uint32 startTime, uint32 cliff, uint32 duration, ) = vesting.info();
+    vm.assume(delta < duration - cliff);
+    vm.warp(startTime + cliff + delta);
+  }
+
+  function gotoFullyVested(uint32 delta) public virtual {
     (uint32 startTime, , uint32 duration, ) = vesting.info();
-    vm.warp(startTime + duration);
+    vm.assume(delta < type(uint32).max - (startTime + duration));
+    vm.warp(startTime + duration + delta);
   }
 }
