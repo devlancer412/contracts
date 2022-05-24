807e9e318d9e11de4ffc56624c879d7639cb1c9c
DevLancer <DevAnderson0412@gmail.com>
2022-05-23 22:00:09 -0400

    test

--- forge_test/test.sh
+++ forge_test/test.sh
@@ -1,5 +1,5 @@
 # Normal test
-forge test --no-match-contract ScholarshipTest -vvvv &&
+forge test --no-match-contract EggSaleTest &&
 
 # Forking test
-forge test --fork-url $(grep POLYGON_RPC_URL .env | cut -d '=' -f2) --match-contract ScholarshipTest -vvvv
+forge test --fork-url $(grep POLYGON_RPC_URL .env | cut -d '=' -f2) --match-contract EggSaleTest
