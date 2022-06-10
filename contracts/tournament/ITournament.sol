23479f80ee0418a63fe93ffe832e7725b487a333
regohiro.eth <62818999+regohiro@users.noreply.github.com>
2022-06-11 01:22:22 +0900

    updated registeration

--- contracts/tournament/ITournament.sol
+++ contracts/tournament/ITournament.sol
@@ -2,19 +2,21 @@
 pragma solidity >=0.8.4;
 
 interface ITournament {
-  // 4slots
+  // 5slots
   struct Game {
     uint32 checkinStartTime; // Registration start date in block.timestamp [4]
     uint32 checkinEndTime; // Registeration end date in block.timestamp [4]
     uint32 gameStartTime; // Tournament start date in block.timestamp [4]
     uint32 gameEndTime; // Tournament end date in block.timestamp [4]
-    uint32 rewardExpiration; // Reward expiration date to claim in block.timestamp [4]
+    uint32 expirationTime; // Expiration date to claim in block.timestamp [4]
     uint32 minRoosters; // Minimum roosters required to start [4]
     uint32 maxRoosters; // Maximum roosters for game [4]
     uint32 roosters; // Number of rooosters [4]
     uint256 entranceFee; // Entrance fee in USDC [32]
-    bytes32 distribution; // Merkle root of prize distribution [32]
-    bytes8 requirementId; // Requirement id [8]
+    bytes32 rankingRoot; // Merkle root of tournament ranking [32]
+    uint16[] distributions; // Array of distrubution percentages in hundreds [32 + 2n]
+    uint16 fee; // Protocol fee in hundreds [4]
+    bytes4 requirementId; // Requirement id [4]
     address organizer; // Organizer [20]
     State state; // Event state [1]
   }
@@ -26,20 +28,22 @@ interface ITournament {
   }
 
   enum State {
-    ALIVE,
+    ONGOING,
     ENDED,
     PAUSED,
     CANCELLED
   }
 
   enum Action {
+    ADD,
     END,
     PAUSE,
+    UNPAUSE,
     CANCEL
   }
 
   event NewGame(uint256 indexed gameId, bytes8 indexed requirementId, address indexed organzier);
-  event SetGame(uint256 indexed gameId, bytes32 distribution);
+  event SetGame(uint256 indexed gameId, Action indexed action);
 
   error InvalidDeadline();
   error InvalidTimeWindow();
--- contracts/tournament/Tournament.sol
+++ contracts/tournament/Tournament.sol
@@ -7,12 +7,15 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import {Scholarship} from "../scholarship/Scholarship.sol";
 
 contract Tournament is ITournament, Auth {
   // Address of USDC
   IERC20 public immutable usdc;
   // Address of rooster
   IERC721 public immutable rooster;
+  // Address of scholarship contract
+  Scholarship public scholarship;
   // Address of vault -- where fees go
   address public vault;
 
@@ -23,16 +26,16 @@ contract Tournament is ITournament, Auth {
   // 1 ~ 2^32 - 2: ranking of winners
   // 2^32 - 1: registered / unranked roosters
   mapping(uint256 => mapping(uint256 => uint32)) public roosters;
-  // Burned nonces
-  mapping(bytes32 => bool) private _nonceBurned;
 
   constructor(
     address usdc_,
     address rooster_,
+    address scholarship_,
     address vault_
   ) {
     usdc = IERC20(usdc_);
     rooster = IERC721(rooster_);
+    scholarship = Scholarship(scholarship_);
     vault = vault_;
   }
 
@@ -43,19 +46,19 @@ contract Tournament is ITournament, Auth {
    */
   function createGame(Game memory game) external onlyRole("MANAGER") returns (uint256 gameId) {
     // Param check
-    require(game.checkinStartTime < game.checkinEndTime, "");
-    require(game.checkinStartTime >= block.timestamp, "");
-    require(game.gameStartTime > game.checkinEndTime, "");
-    require(game.gameStartTime < game.gameEndTime, "");
+    require(game.checkinStartTime < game.checkinEndTime, "Invalid checkin time window");
+    require(game.checkinStartTime >= block.timestamp, "Invalid checkin start time");
+    require(game.gameStartTime < game.gameEndTime, "Invalid game time window");
+    require(game.gameStartTime > game.checkinEndTime, "Invalid game time window");
 
     // Get game id
     gameId = games.length;
 
     // Create game
     game.roosters = 0;
-    game.distribution = bytes32(0);
+    game.rankingRoot = bytes32(0);
     game.organizer = msg.sender;
-    game.state = State.ALIVE;
+    game.state = State.ONGOING;
     games.push(game);
 
     emit NewGame(gameId, game.requirementId, msg.sender);
@@ -65,47 +68,72 @@ contract Tournament is ITournament, Auth {
    * @notice Sets state of game
    * @param action Action enum
    * @param gameId Game id
-   * @param distribution Merkle root of prize distrubution
+   * @param rankingRoot Merkle root of ranking (optional)
+   * @param distributions Distrubtion percentages to add (optional)
    */
   function setGame(
     Action action,
     uint256 gameId,
-    bytes32 distribution
+    bytes32 rankingRoot,
+    uint16[] calldata distributions
   ) external {
     Game storage game = games[gameId];
 
     // Access check
-    require(msg.sender == game.organizer, "");
+    require(msg.sender == game.organizer, "Not organizer");
 
+    if (action == Action.ADD) {
+      uint256 n = distributions.length;
+      require(block.timestamp < game.checkinStartTime, "Signup started");
+      require(n > 0, "distrubutions not provided");
+      for (uint256 i = 0; i < n; i++) {
+        game.distributions.push(distributions[i]);
+      }
+    }
     if (action == Action.END) {
-      require(block.timestamp >= game.gameEndTime, "");
-      require(distribution != bytes32(0), "");
-      game.distribution = distribution;
+      require(block.timestamp >= game.gameEndTime, "Game ongoing");
+      require(rankingRoot != bytes32(0), "rankingRoot not provided");
+      game.rankingRoot = rankingRoot;
       game.state = State.ENDED;
     } else if (action == Action.CANCEL) {
-      require(game.state != State.ENDED, "");
+      require(game.state == State.ONGOING, "Game ended");
       game.state = State.CANCELLED;
     } else if (action == Action.PAUSE) {
+      require(game.state == State.ONGOING, "Game ended");
       game.state = State.PAUSED;
+    } else if (action == Action.UNPAUSE) {
+      require(game.state != State.PAUSED, "Not paused");
+      game.state = State.ONGOING;
     }
 
-    emit SetGame(gameId, distribution);
+    emit SetGame(gameId, action);
   }
 
+  /**
+   * @notice Registers for the tournament game
+   * @param gameId Game id
+   * @param roosterIds List of roosters to register
+   * @param sig Signature for tournament qualification
+   */
   function register(
     uint256 gameId,
-    uint256 roosterId,
-    bytes32 nonce,
+    uint256[] calldata roosterIds,
     Sig calldata sig
   ) external {
     Game storage game = games[gameId];
+    uint256 n = roosterIds.length;
+    require(block.timestamp >= game.checkinStartTime, "Not started");
+    require(block.timestamp < game.checkinEndTime, "Ended");
+    require(n > game.maxRoosters - game.roosters, "Reached limit");
+    require(_isOwner(msg.sender, roosterIds), "Not owner");
+    require(_isQualified(gameId, game.requirementId, roosterIds, sig), "Not qualified");
 
-    if (_canRegister(gameId, roosterId) == false) revert();
-    if (_isQualified(roosterId, nonce, sig) == false) revert();
+    for (uint256 i = 0; i < n; i++) {
+      roosters[gameId][roosterIds[i]] = type(uint32).max;
+    }
+    game.roosters += uint32(n);
 
-    roosters[gameId][roosterId] = type(uint32).max;
-
-    usdc.transferFrom(msg.sender, address(this), game.entranceFee);
+    usdc.transferFrom(msg.sender, address(this), game.entranceFee * n);
   }
 
   function claim(
@@ -118,9 +146,9 @@ contract Tournament is ITournament, Auth {
     Game storage game = games[gameId];
     bytes32 node = keccak256(abi.encodePacked(gameId, roosterId, amount, ranking));
 
-    if (game.distribution == bytes32(0)) revert();
+    if (game.rankingRoot == bytes32(0)) revert();
     if (roosters[gameId][roosterId] != type(uint64).max) revert();
-    if (MerkleProof.verify(proof, game.distribution, node)) revert();
+    if (MerkleProof.verify(proof, game.rankingRoot, node)) revert();
 
     roosters[gameId][roosterId] = ranking;
 
@@ -128,11 +156,29 @@ contract Tournament is ITournament, Auth {
     usdc.transfer(msg.sender, (amount * 9) / 10);
   }
 
-  function _canRegister(uint256 gameId, uint256 roosterId) private returns (bool) {}
+  function _isOwner(address owner, uint256[] calldata roosterIds) private view returns (bool) {
+    for (uint256 i = 0; i < roosterIds.length; i++) {
+      if (
+        rooster.ownerOf(roosterIds[i]) != owner || scholarship.nft_owner(roosterIds[i]) != owner
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
 
   function _isQualified(
-    uint256 roosterId,
-    bytes32 nonce,
+    uint256 gameId,
+    bytes8 requirementId,
+    uint256[] calldata roosterIds,
     Sig calldata sig
-  ) private returns (bool) {}
+  ) private view returns (bool) {
+    bytes32 messageHash = keccak256(abi.encodePacked(gameId, requirementId, roosterIds));
+    bytes32 ethSignedMessageHash = keccak256(
+      abi.encodePacked("\x19Ethereum Signed Message:\n32", messageHash)
+    );
+    return hasRole("SIGNER", ecrecover(ethSignedMessageHash, sig.v, sig.r, sig.s));
+  }
+
+  function set() external onlyOwner {}
 }
