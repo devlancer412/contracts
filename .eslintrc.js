98220bff0e563a3a393eac55ac43eeeb29ca750e
regohiro.eth <62818999+regohiro@users.noreply.github.com>
2022-06-14 17:00:41 +0900

    allow let

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,6 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/no-unused-vars": "off",
+    "prefer-const": "off",
   },
 };
