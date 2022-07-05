1e4674b47b60ce9d941bc03bca922f443829e408
DevLancer <DevAnderson0412@gmail.com>
2022-07-05 01:41:23 -0400

    testing

--- scripts/affiliate.ts
+++ scripts/affiliate.ts
@@ -1,4 +1,4 @@
-import { Interface } from "ethers/lib/utils";
+import { Interface, solidityKeccak256 } from "ethers/lib/utils";
 import { Contract } from "ethers";
 import {
   MockUsdc__factory,
@@ -12,23 +12,31 @@ import { abiEgg, abiStore, realAbiEgg, realAbiStore } from "../test/AffiliateAbi
 const main = async () => {
   const { connect, provider, accounts } = await Ship.init();
 
+  console.log("preparing....");
   const usdc = await connect(MockUsdc__factory);
   const egg = await connect(RoosterEgg__factory);
   const eggsale = await connect(RoosterEggSale__factory);
   const affiliate = await connect(Affiliate__factory);
   await eggsale.setAffiliateData(affiliate.address, 50);
 
+  if ((await egg.owner()) != eggsale.address) {
+    console.log("transferring ownership of egg");
+    const tx = await egg.connect(accounts.deployer).transferOwnership(eggsale.address);
+    await tx.wait();
+  }
   const proxyContract = new Contract(affiliate.address, abiEgg, provider);
   const iRealFace = new Interface(realAbiEgg);
 
   console.log(await provider.getBalance(accounts.alice.address));
-
   await usdc.connect(accounts.alice).approve(eggsale.address, 500);
+
+  const affiliateId = solidityKeccak256(["address"], [accounts.bob.address]);
+
   const tx = await proxyContract.connect(accounts.alice).buyEggWithAffiliate(
     accounts.alice.address, // to address to send egg
     10, // amount of egg
     eggsale.address, // eggsale contract address
-    accounts.bob.address, // affiliate address
+    affiliateId, // affiliate address
     iRealFace.getSighash("buyEggWithAffiliate"), // function selector to replace
   );
 
